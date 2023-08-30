31aca1a7996a522cdab799c4731bf337daedcf34
Alex Lyttle <a.j.lyttle@outlook.com>
2023-08-30 17:09:43 +0100

    Add parameters

--- src/stelevator/parameters.py
+++ src/stelevator/parameters.py
@@ -1,113 +1,85 @@
 import numpy as np
+import astropy.units as u
+from .utils import _ListSameType
 
 
 class Parameter(object):
     """Base class for parameters.
-    
+
     Args:
         name (str): Name of the parameter.
         symbol (str): Symbol of the parameter. Defaults to None.
-        unit (str): Unit of the parameter. Defaults to None.
-        bounds (object): Bounds of the parameter. Defaults to None.
+        unit (str or astropy.units.Unit): Unit of the parameter. Defaults to None.
         desc (str): Description of the parameter. Defaults to None.
     """
-    def __init__(self, name, symbol=None, unit=None, bounds=None, desc=None):
+    def __init__(self, name, symbol=None, unit=None, desc=None):
         self.name = name
-        self.unit = unit
-        self.bounds = (-np.inf, np.inf) if bounds is None else bounds
-        self.symbol = symbol
-        self.desc = desc
+        self.unit = u.Unit('' if unit is None else unit)
+        self.symbol = r'\mathrm{' + name + '}' if symbol is None else symbol
+        self.desc = name if desc is None else desc
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', symbol='{self.symbol}', unit='{self.unit.to_string()}', desc='{self.desc}')"
 
 
-class Initial(Parameter):
+class _ParameterWrapper(Parameter):
+    """Abstract base class for parameter wrappers."""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class Initial(_ParameterWrapper):
+    """Base class for initial parameters."""
     def __init__(self, parameter: Parameter):
         desc = parameter.desc[0].lower() + parameter.desc[1:]
         super().__init__(
             '_'.join([parameter.name, 'init']), 
             '_'.join([parameter.symbol, r'\mathrm{init}']),
             parameter.unit,
-            parameter.bounds,
             f"Initial {desc}."
         )
 
 
-class Surface(Parameter):
+class Surface(_ParameterWrapper):
+    """Base class for surface parameters."""
     def __init__(self, parameter: Parameter):
+        self._parameter = parameter
         desc = parameter.desc[0].lower() + parameter.desc[1:]
         super().__init__(
             '_'.join([parameter.name, 'surf']), 
             '_'.join([parameter.symbol, r'\mathrm{surf}']),
             parameter.unit,
-            parameter.bounds,
             f"Surface {desc}."
         )
 
 
-class Mass(Parameter):
-    def __init__(self, unit='Msun', bounds=None):
-        desc = "Stellar mass."
-        super().__init__('mass', 'M', unit, bounds, desc)
+class Log10(_ParameterWrapper):
+    def __init__(self, parameter: Parameter):
+        desc = parameter.desc[0].lower() + parameter.desc[1:]
+        super().__init__(
+            '_'.join(['log', parameter.name]), 
+            rf'\log({parameter.symbol})',
+            u.dex(parameter.unit),
+            f"Log base-10 {desc}."
+        )
 
 
-class Age(Parameter):
-    def __init__(self, unit='Gyr', bounds=None):
-        desc = "Stellar age."
-        super().__init__('age', 't', unit, bounds, desc)
+class ParameterList(_ListSameType):
+    """List of Parameter objects."""
+    def __init__(self, parameters=None):
+        super().__init__(Parameter, data=parameters)
 
 
-class Y(Parameter):
-    def __init__(self, unit=None, bounds=None):
-        desc = "Initial stellar helium mass fraction."
-        super().__init__('Y', 'Y', unit, bounds, desc)
-
-
-class Z(Parameter):
-    def __init__(self, unit=None, bounds=None):
-        desc = "Initial stellar heavy element mass fraction."
-        super().__init__('Z', 'Z', unit, bounds, desc)
-
-
-class MixingLength(Parameter):
-    def __init__(self, unit=None, bounds=None):
-        super().__init__('a_MLT', r'\alpha_\mathrm{MLT}', unit, bounds)
-
-
-class MH(Parameter):
-    def __init__(self, unit=None, bounds=None):
-        super().__init__('M_H', r'[\mathrm{M}/\mathrm{H}]', unit, bounds)
-
-
-class Radius(Parameter):
-    def __init__(self, unit="Rsun", bounds=None):
-        desc = "Stellar radius."
-        super().__init__('radius', 'R', unit, bounds, desc)
-
-
-class Luminosity(Parameter):
-    def __init__(self, unit="Lsun", bounds=None):
-        desc = "Stellar luminosity."
-        super().__init__('luminosity', 'L', unit, bounds, desc)
-
-
-class Teff(Parameter):
-    def __init__(self, unit="K", bounds=None):
-        desc = "Stellar effective temperature."
-        super().__init__('Teff', r'T_\mathrm{eff}', unit, bounds, desc)
-
-
-class Logg(Parameter):
-    def __init__(self, unit=None, bounds=None):
-        desc = "Stellar surface gravity."
-        super().__init__('log_g', r'\log(g)', unit, bounds, desc)
-
-
-class DeltaNu(Parameter):
-    def __init__(self, unit="muHz", bounds=None):
-        desc = "Asteroseismic large frequency separation."
-        super().__init__('DeltaNu', r'\Delta\nu', unit, bounds, desc)
-
-
-class PRot(Parameter):
-    def __init__(self, unit="days", bounds=None):
-        desc = "Stellar rotation period."
-        super().__init__('PRot', r'P_\mathrm{rot}', unit, bounds, desc)
+mass = Parameter('mass', 'M', 'Msun', desc='Stellar mass')
+age = Parameter('age', 't', 'Gyr', desc='Stellar age')
+helium_mass_frac = Parameter('helium_mass_frac', 'Y', desc='Stellar helium mass fraction')
+metal_mass_frac = Parameter('metal_mass_frac', 'Z', desc='Stellar heavy element mass fraction')
+mixing_length_param = Parameter('mixing_length_param', r'\alpha_\mathrm{MLT}', desc='Mixing length parameter')
+metal_over_hydrogen = Parameter('metal_over_hydrogen', r'[\mathrm{M}/\mathrm{H}]', 'dex', desc='Metallicity')
+radius = Parameter('radius', 'R', 'Rsun', desc='Stellar radius')
+luminosity = Parameter('luminosity', 'L', 'Lsun', desc='Stellar luminosity')
+effective_temperature = Parameter('effective_temperature', r'T_\mathrm{eff}', 'K', desc='Stellar effective temperature')
+large_freq_sep = Parameter('large_freq_sep', r'\Delta\nu', 'uHz', desc='Asteroseismic large frequency separation')
+rotation_period = Parameter('rotation_period', r'P_\mathrm{rot}', 'day', desc='Stellar rotation period')
+surface_gravity = Parameter('surface_gravity', 'g', 'cm/s2', desc='Stellar surface gravity')
+frac_evol = Parameter('frac_evol', r'f_\mathrm{evo}', desc='Fractional evolutionary phase')
