7f0eef9549345a960cf6a1e86ee75ef5fa89c49c
Alex Lyttle <a.j.lyttle@outlook.com>
2023-09-04 17:45:08 +0100

    Add draft docstring replacer

--- src/stelevator/utils.py
+++ src/stelevator/utils.py
@@ -7,3 +7,13 @@ class _ListSameType(UserList):
         super().__init__(data)
         if any(not isinstance(d, dtype) for d in self.data):
             raise TypeError(f"All parameters must an instance of '{dtype.__class__.__name__}'.")
+
+
+# def replace_docstring(oldvalue, newvalue):
+#     """Replace 'oldvalue' with 'newvalue' in the docstring of decorated function."""
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             return func(*args, **kwargs)
+#         wrapper.__doc__ = func.__doc__.replace(oldvalue, newvalue)
+#         return wrapper
+#     return decorator
